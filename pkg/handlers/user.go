8a6ea3f41ba51f80b7559f29534ea664df5b9c62
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-04-09 10:49:30 +0530

    fix:  extracting the user creation and update logic into separate methods for better readability and maintainability

--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -1,9 +1,8 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/sandeep-jaiswar/osiris-backend-vault/pkg/logger"
@@ -22,40 +21,58 @@ func NewUserHandler(userService *service.UserService) *UserHandler {
 	return &UserHandler{userService}
 }
 
+// UpsertUserHandler handles the creation or update of a user
 func (uh *UserHandler) UpsertUserHandler(w http.ResponseWriter, r *http.Request) {
-    var newUser models.User
-    err := json.NewDecoder(r.Body).Decode(&newUser)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	var newUser models.User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    ctx := r.Context()
+	ctx := r.Context()
 
-    existingUser, err := uh.userService.GetUserByEmail(ctx, newUser.Email)
-    if err != nil && err.Error() != service.ErrUserNotFound.Error() {
-        fmt.Printf("Error: %v\n", err)
-        fmt.Printf("service Error: %v\n", service.ErrUserNotFound)
-        fmt.Printf("compare: %v\n", errors.Is(err, service.ErrUserNotFound))
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	existingUser, err := uh.userService.GetUserByEmail(ctx, newUser.Email)
+	if err != nil {
+		if err.Error() != service.ErrUserNotFound.Error() {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 
-    if existingUser != nil {
-        w.WriteHeader(http.StatusOK)
-        json.NewEncoder(w).Encode(existingUser)
-        logger.Log.Info("user is updated", zap.Uint("id", existingUser.ID))
-    } else {
-        err = uh.userService.CreateUser(ctx, &newUser)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-
-        // Set status code for successful creation
-        w.WriteHeader(http.StatusCreated)
-        json.NewEncoder(w).Encode(newUser)
-        logger.Log.Info("user is created", zap.Uint("id", newUser.ID))
-    }
+	if existingUser != nil {
+		err = uh.updateUser(w, existingUser)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		err = uh.createUser(w, ctx, &newUser)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
+// updateUser updates an existing user
+func (uh *UserHandler) updateUser(w http.ResponseWriter, existingUser *models.User) error {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(existingUser)
+	logger.Log.Info("user is updated", zap.Uint("id", existingUser.ID))
+	return nil
+}
+
+// createUser creates a new user
+func (uh *UserHandler) createUser(w http.ResponseWriter, ctx context.Context, newUser *models.User) error {
+	err := uh.userService.CreateUser(ctx, newUser)
+	if err != nil {
+		return err
+	}
+
+	// Set status code for successful creation
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
+	logger.Log.Info("user is created", zap.Uint("id", newUser.ID))
+	return nil
+}
